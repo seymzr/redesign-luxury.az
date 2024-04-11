1571bd3e52a9b3bf8aba3cb3d4889f08aeebe196
seymzr <seymzr@gmail.com>
2024-04-11 22:06:15 +0400

    s

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom"
 import About from "./Pages/About"
 import Home from "./Pages/Home"
+import Services from "./Pages/Services"
+import Navbar from "./components/navbar"
+import Footer from "./components/Footer"
 
 
 
@@ -9,10 +12,13 @@ function App() {
 
   return (
     <BrowserRouter>
+    <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="about"  element={<About />}/>
+        <Route path="services"  element={<Services />}/>
       </Routes>
+      <Footer />
     </BrowserRouter>
   )
 }
--- src/icons/logo.tsx
+++ src/icons/logo.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Logo = () => {
   return (
-   <img className="w-96 grayscale brightness-0" src="/logo.png" alt="logo" />
+   <img className="w-48 md:w-96 grayscale brightness-0" src="/logo.png" alt="logo" />
   );
 };
 
