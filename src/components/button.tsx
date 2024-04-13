10c2ca57daad8777ca4be256e0782fd59f5d6a3c
seymzr <seymzr@gmail.com>
2024-04-13 13:05:12 +0400

    s

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -11,8 +11,8 @@ const Button = (props: ButtonProps) => {
   const { variant = "primary", text = "Default Text", type = "button" } = props;
 
   const defaultStyles =
-    "rounded-[18px] flex capitalize items-center justify-center gap-[9px] w-fit text-primary-200 duration-300 hover:text-white text-base lg:text-lg font-semibold font-['Jost'] leading-snug tracking-tight px-10 py-5 md:px-[54px] md:py-[26px]";
-  const bgClass = variant === "primary" ? "bg-primary-200 " : "bg-primary-300 hover:bg-primary-100";
+    "rounded-[18px] flex capitalize items-center justify-center gap-[9px] w-fit duration-300  text-base lg:text-lg font-semibold font-['Jost'] leading-snug tracking-tight px-10 py-5 md:px-[54px] md:py-[26px]";
+  const bgClass = variant === "primary" ? "bg-primary-200 text-primary-100" : "bg-primary-300 text-primary-200 hover:text-white hover:bg-primary-100";
   const className = `${defaultStyles} ${bgClass} `;
 
   return (
