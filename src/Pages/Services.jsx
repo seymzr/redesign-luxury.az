dfcd4de25b63c33509e21ca092d2844fcb6f40c5
seymzr <seymzr@gmail.com>
2024-05-24 22:05:03 +0400

    s

--- src/Pages/Services.jsx
+++ src/Pages/Services.jsx
@@ -43,29 +43,29 @@ const Services = () => {
       imgIcon: ServiceIconOne,
       numIcon: One,
       img: "/service1.png",
-      title: "Concept & Details",
-      info: "It is a long established fact  will be distracted. Lorem Ipsum is simply dummy from text of the and typesetting indufstry.",
+      title: "İlkin mərhələ",
+      info: "Baxış və ölçünün götürülməsi üçün mənzilə(obyektə) gedilir. Sifarişçinin istəkləri nəzərə alınaraq bir neçə versiyada bölgü planı tərtib olunur.  Sifarişçi tərəfindən seçilən bölgü planı üzərində yerləşim planı hazırlanır. ",
     },
     {
       imgIcon: ServiceIconTwo,
       numIcon: Two,
       img: "/service2.png",
-      title: "Idea For Work",
-      info: "It is a long established fact  will be distracted. Lorem Ipsum is simply dummy from text of the and typesetting indufstry.",
+      title: "Dizayn mərhələsi",
+      info: "Dizayn hər bir sifarişçiyə individual olaraq hazırlanır. Mükəmməl dizaynla yanaşı ,  funksional və rahat interyer təqdim edilir.  Hər bir detalın sifarişçiyə özəl olması üçün zövq və istəklər nəzərə alınır. İstəklərə uyğun olaraq standartlardan kənar və gözoxşayan dizayn hazırlanır.",
     },
     {
       imgIcon: ServiceIconThree,
       numIcon: Three,
       img: "/service3.png",
-      title: "Design",
-      info: "It is a long established fact  will be distracted. Lorem Ipsum is simply dummy from text of the and typesetting indufstry.",
+      title: "Təmir mərhələsi",
+      info: "Dizaynın reallaşması üçün ən vacib və qəliz mərhələlərdən biri təmirdir. Ustalarımız işinin peşəkarı mühəndislərdir. Keyfiyyətli və səliqəli təmir üçün işinizi peşəkarlara həvalə edin.",
     },
     {
       imgIcon: ServiceIconFour,
       numIcon: Four,
       img: "/service4.png",
-      title: "Perfection",
-      info: "It is a long established fact  will be distracted. Lorem Ipsum is simply dummy from text of the and typesetting indufstry.",
+      title: "Mebel və aksessuar mərhələsi",
+      info: "İnteryerin dizaynla birə-bir uyğunlaşması üçün mebel və aksessuar seçimi düzgün edilməlidir. Sifarişçinin komfortlu və estetik interyerə sahib olması üçün ən xırda detal belə diqqətlə seçilir.",
     },
     
   ];
@@ -114,7 +114,7 @@ const Services = () => {
       <section className="px-12 xl:px-0 w-full mt-[100px] lg:mt-[200px] min-h-screen ">
         <div className="w-full lg:max-w-[1200px] p-12 mx-auto lg:px-[116px] lg:py-[160px] bg-primary-300 rounded-[70px] ">
           <h2 className="font-dm tracking-wide text-center leading-[37px] lg:leading-[62.50px] text-[30px] lg:text-[50px] w-full capitalize lg:max-w-[50%] pb-2 mx-auto">
-            How We Work
+            Biz necə İşləyirik
           </h2>
           <p className="text-base md:text-[22px] text-center tracking-tight font-jost text-text-gray leading-[33px] lg:max-w-[60%] mx-auto">
             It is a long established fact that a reader will be distracted by
