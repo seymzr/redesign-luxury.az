e9b3b39308fb698c011ec03420b68ee592614a52
seymzr <seymzr@gmail.com>
2024-05-24 22:05:02 +0400

    s

--- src/components/contact-box.jsx
+++ src/components/contact-box.jsx
@@ -5,14 +5,14 @@ import Web from "../icons/web.tsx";
 const ContactInfo = () => {
   return (
     <div className="col-span-1 bg-primary-300 rounded-[30px] px-8 py-[76px] flex flex-col gap-[30px] w-full">
-      <h5 className="flex flex-col xl:flex-row gap-4 items-center text-xl font-jost tracking-tight">
+      <a href="mailto:info@redesignluxury.az" className="flex flex-col xl:flex-row gap-4 items-center text-xl font-jost tracking-tight">
         {" "}
-        <Mail /> info@yourdomain.com
-      </h5>
-      <h5 className="flex flex-col xl:flex-row gap-4 items-center text-xl font-jost tracking-tight">
+        <Mail /> info@redesignluxury.az
+      </a>
+      <a href="tel:+994502555200" className="flex flex-col xl:flex-row gap-4 items-center text-xl font-jost tracking-tight">
         {" "}
         <Phone /> +994 50 255 52 00
-      </h5>
+      </a>
       
     </div>
   );
