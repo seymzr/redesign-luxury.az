0ac691c712564c71b93462a4213778a32eb059c6
seymzr <seymzr@gmail.com>
2024-05-25 09:29:10 +0400

    s

--- src/Pages/Gallery.jsx
+++ src/Pages/Gallery.jsx
@@ -1,58 +1,98 @@
+import { useEffect, useState } from "react";
+import Masonry,{ResponsiveMasonry} from "react-responsive-masonry";
+
 
 export default function Gallery() {
+  const images = [
 
+      "gallery1.jpg",
+      "gallery2.png",
+    "gallery3.png",
+    "gallery4.png",
+    "gallery5.png",
+    "gallery6.png",
+    "gallery7.jpg",
+    "gallery8.png",
+    "gallery9.png",
+    "gallery10.png",
+    "gallery11.png",
+    "gallery12.png",
+    "gallery13.png",
+    "gallery14.png",
+    "gallery15.png",
+    "gallery16.png",
+    "gallery17.png",
+    "gallery18.png",
+    "gallery19.png",
+    "gallery20.png",
+    "gallery21.png",
+    "gallery22.png",
+    "gallery23.png",
+    "gallery24.png",
+    "gallery25.png",
+    "gallery26.png",
+    "gallery27.png",
+    "gallery28.png",
+    "gallery29.png",
+    "gallery30.jpg",
+    "gallery31.jpg",
+    "gallery32.png",
+    "gallery33.png",
+    "gallery34.jpg",
+    "gallery35.png",
+    "gallery36.jpg",
+    "gallery37.png",
+    "gallery38.jpg",
+    "gallery39.png",
+    "gallery40.png",
+    "gallery41.jpg",
+    "gallery42.jpg",
+    "gallery43.jpg",
+    "gallery44.png",
+    "gallery45.png",
+    "gallery46.png",
+    "gallery47.png",
+    "gallery48.jpg",
+    "gallery49.jpg",
+    "gallery50.jpg",
+    "gallery51.jpg",
+    "gallery52.jpg",
+    "gallery53.jpg",
+    "gallery54.jpg",
+    "gallery55.png",
+    "gallery56.png",
+    "gallery57.png",
+    "gallery59.png",
+    "gallery60.png",
+    "gallery61.png",
+  ];
+  const [files, setFiles] = useState();
+  useEffect(() => {
+    fetch("./gallery")
+      .then((response) => response)
+      .then((data) => setFiles(data))
+      .catch((error) => console.error("Error fetching gallery files:", error));
+  }, []);
+  useEffect(() => {
+    console.log(files);
+  }, [files]);
 
-    return (
+  return (
+    <div className="container mx-auto py-10  gap-4 px-5">
+      <ResponsiveMasonry  columnsCountBreakPoints={{350: 1, 750: 2, 900: 4}}>
+      <Masonry className="w-full"  style={{width:"100%"}}>
+      {images?.map((file, index) => (
+        <img
+          key={index}
+          src={`/gallery/${file}`}
+          alt={file}
+          className="gallery-image rounded-lg m-3"
+        />
+      ))}
+      </Masonry>
+      </ResponsiveMasonry>
 
-        <div className="container mx-auto py-10 grid grid-cols-2 md:grid-cols-4 gap-4 px-5">
-
-
-
-            <div className="flex flex-col gap-4">
-                <div>
-                    <img className="h-auto max-w-full rounded-lg" src="/bedroom.jpg" alt="" />
-                </div>
-                <div>
-                    <img className="h-auto max-w-full rounded-lg" src="/gallery2.jpg" alt="" />
-                </div>
-                <div>
-                    <img className="h-auto max-w-full rounded-lg" src="/kitchen1.jpg" alt="" />
-                </div>
-            </div>
-            <div className="flex flex-col gap-4">
-                <div>
-                    <img className="h-auto max-w-full rounded-lg" src="/swiper1.jpg" alt="" />
-                </div>
-                <div>
-                    <img className="h-auto max-w-full rounded-lg" src="/drowing.jpg" alt="" />
-                </div>
-                <div>
-                    <img className="h-auto max-w-full rounded-lg" src="/living.jpg" alt="" />
-                </div>
-            </div>
-            <div className="flex flex-col gap-4">
-                <div>
-                    <img className="h-auto max-w-full rounded-lg" src="/gallery14.jpg" alt="" />
-                </div>
-                <div>
-                    <img className="h-auto max-w-full rounded-lg" src="/gallery15.jpg" alt="" />
-                </div>
-                <div>
-                    <img className="h-auto max-w-full rounded-lg" src="/gallery12.jpg" alt="" />
-                </div>
-            </div>
-            <div className="flex flex-col gap-4">
-                <div>
-                    <img className="h-auto max-w-full rounded-lg" src="/gallery7.jpg" alt="" />
-                </div>
-                <div>
-                    <img className="h-auto max-w-full rounded-lg" src="/gallery11.jpg" alt="" />
-                </div>
-                <div>
-                    <img className="h-auto max-w-full rounded-lg" src="/gallery3.jpg" alt="" />
-                </div>
-            </div>
-        </div>
-
-    );
+     
+    </div>
+  );
 }
